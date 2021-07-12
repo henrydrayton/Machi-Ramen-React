03623e7107df98fa8db5ca0babc839067270bdf4
henrydrayton <henrydrayton0@gmail.com>
2021-07-12 18:09:57 +1000

    create nav bar

---
+++ src/components/Main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main>
+            <div>
+                <div>
+                    <h2>Machi Ramen</h2>
+                    <h4>We are formed based on the philosophy of finding, preserving and spreading original flavors to everyone.</h4>
+                </div>
+                <div>
+                    <h3>Japanese Street Food Specalists </h3>
+                    <h4>We are formed based on the philosophy of finding, preserving and spreading original flavors to everyone.</h4>
+                </div>
+                <div>
+                    <h2>Family Run</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+                <div>
+                    <h2>Authentic</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+                <div>
+                    <h2>Cozy</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
