b772d400423c5a5907409de9673e3388cf23ab5e
henrydrayton <henrydrayton0@gmail.com>
2021-07-13 22:53:55 +1000

    add font-families lao MN and poppins

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,23 @@ import React from 'react'
 function Contact() {
     return (
         <div>
-            <h1>Contact</h1>
+            <div>
+                <h1>Get In Touch</h1>
+            </div>
+            <div>
+                <h4>Phone Number</h4>
+                <p>1234567890</p>
+            </div>
+            <div>
+                <h4>Email</h4>
+                <p>test@test.com</p>
+            </div>
+            <div>
+                <h4>Address</h4>
+                <p>123 Street, Ultimo</p>
+            </div>
         </div>
+
     );
 }
 
