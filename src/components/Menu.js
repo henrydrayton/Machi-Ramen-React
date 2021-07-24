ccf2e4f0a26ad937b4fd65f166de2478d1492a01
henrydrayton <henrydrayton0@gmail.com>
2021-07-24 18:15:43 +1000

    style menu cards, to be in grids later

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -15,19 +15,32 @@ function Menu() {
 // This boolean operator checks whether items are present. 
 // If there are no items to display, the function will return 'Loading...'
 // iterates over each item in the database and displays its individual attribute
+
 			items.length ?
 			(
-				<div>
-					{items.map(item => (
+					<div>
+						<h1 className="title">Menu</h1>
+						{items.map(item => (
 						<div key={item.id}>
-							<h3>Name: <Link to={`/items/${item.id}`}>{item.name}</Link></h3>
-							<p>Price: {item.price}</p>
-							<p>Description: {item.description}</p>
-							<p>Category ID: {item.category_id}</p>
-							<p>Image: <img src={item.image_url} alt="food"/></p>
+							<div className="card-container">
+									<div className="md:flex shadow-lg  mx-6 md:mx-auto my-20 max-w-lg md:max-w-2xl h-64">
+										<p><img src={item.image_url} className="h-full w-full md:w-3/3  object-cover rounded-lg rounded-r-none pb-5/6" alt="food"/></p>
+										<div className="w-full md:w-2/3 px-4 py-4 bg-white rounded-lg">
+											<div className="flex items-center">
+												<h2><Link to={`/items/${item.id}`} className="text-xl text-gray-800 font-medium mr-auto">{item.name}</Link></h2>
+												<div className="price">
+												<p className="text-gray-800 font-semi tracking-tighter">Price: {item.price}</p>
+												</div>
+											</div>
+											<p className=" description text-sm text-gray-700 mt-4">{item.description}</p>
+											{/* <p>Category ID: {item.category_id}</p> */}
+										</div>
+									</div>
+							</div>
 						</div>
-					))}
-				</div>
+							))}
+					</div>
+				
 			) : (
 				<div>Loading&hellip;</div>
 
